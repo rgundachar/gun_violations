bf37cb18d3704098e819211fddb161876c0a7266
rgundachar <44592031+rgundachar@users.noreply.github.com>
2019-01-16 11:20:27 -0500

    deleting apit key

--- gun_violations/src/main/jupyter/com/rutgers/gunvio/utils/config.py
+++ gun_violations/src/main/jupyter/com/rutgers/gunvio/utils/config.py
@@ -1,4 +1,3 @@
 # Enter your API key
-#gkey = "AIzaSyCi4656sf1cIqXdT4bSUTTK5mG_Nl4zQC8"
-gkey = "AIzaSyAEVv1W_VjHE8Eud-gQxVNdnkPwku3bsu8"
-gkey = "AIzaSyC3m4W705TqpolNSkl2zwXRNWtq25jne_A"
+
+gkey = "your API key HERE"
